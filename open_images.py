9e45dfdd87520ded45f7124e2f3757fba9bc5463
Trisha Andrews <andrews.trisha.a@gmail.com>
2015-07-25 12:23:16 -0400

    can now get image labels

--- open_images.py
+++ open_images.py
@@ -1,27 +1,39 @@
-#import Image #as pil
 import numpy as np
 from functools import partial
 import cv2
 from cv2 import cv
 
-infile = "train_X.bin"
+xfile = "stl10/train_X.bin"
+yfile = "stl10/train_y.bin"
 imdim = 96
 imsize = imdim * imdim
 chan = 3
 
-with open(infile, 'rb') as f:
-    count = 0
-    lim = 50
-    for chunk in iter(partial(f.read, imsize*chan), ''):
-        img = np.zeros((imdim, imdim, chan), np.uint8)
-        for e in range(chan):
-            a = np.fromstring(chunk[e*imsize:(e+1)*imsize], dtype=np.uint8)
-            im = a.reshape((imdim, imdim)).transpose()
-            img[:,:,-(e+1)] = im
-        
-        cv2.imshow("img", img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        count +=1
-        if count >= lim:
-            break
+def get_label(off, yf=yfile):
+    with open(yf, 'rb') as f2:
+        f2.seek(off)
+        n = f2.read(1)
+        name = np.fromstring(n, dtype=np.uint8)
+        nm =  name[0].astype(int)
+        return str(nm)
+
+def open_image(xf=xfile, show=False):
+    with open(xf, 'rb') as f:
+        count = 0
+        lim = 2
+        for chunk in iter(partial(f.read, imsize*chan), ''):
+            img = np.zeros((imdim, imdim, chan), np.uint8)
+            for e in range(chan):
+                a = np.fromstring(chunk[e*imsize:(e+1)*imsize], dtype=np.uint8)
+                im = a.reshape((imdim, imdim)).transpose()
+                img[:,:,-(e+1)] = im
+            if show:
+                cv2.imshow(get_label(count), img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+            count +=1
+            if count >= lim:
+                break
+
+
+open_image(show=True)
